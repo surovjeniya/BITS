3175c3d13bc87cf7db4417cc500a1f8f9dbf6109
Evgeniy Surov <surovjeniya@gmail.com>
2022-05-16 16:26:54 +0300

    init project

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const cors = require("cors");
+const router = require("./routes/index");
+const cookieparser = require("cookie-parser");
+
+const app = express();
+app.use(cors());
+app.use(cookieparser());
+app.use(express.json());
+app.use("/api", router);
+
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
+const start = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    app.listen(PORT, () => {
+      console.log("Server has been started on:", PORT);
+    });
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+start();
